e14c53880ad6dfa841c3067b92f0e17918de09bb
Manish Malav <malavm206@gmail.com>
2022-03-28 23:17:08 +0530

    chat component added and code running

---
+++ src/Chat.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Chat({socket, username, room}) {
+
+    return (
+         <div>
+              <div className="chat-header">
+              <p>Live Chat</p>
+            </div>
+          <div className="chat-body"></div>
+          <div className="chat-footer">
+              <input type="text" placeholder="Hey...." />
+              <button> #456</button>
+          </div>
+         </div>
+    )
+}
+
+export default Chat;
