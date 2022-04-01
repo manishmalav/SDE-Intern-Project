b3c6b4fcf735f9172af42fbece251513005c1739
Manish Malav <malavm206@gmail.com>
2022-04-01 14:05:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -8,21 +8,31 @@ function App() {
 
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
 
   const joinRoom = () => {
     if(username !== "" && room !== "")
     {
       socket.emit("join_room", room);
+      setShowChat(true);
     }
 
   };
   return (
+
     <div className="App">
+    { !showChat ? (
+      <div className = "joinChatContainer">
      <h3> Join a Chat</h3>
      <input type="text" placeholder="Manish..." onChange={(event) => {setUsername(event.target.value);}}/>
      <input type="text" placeholder="Room ID..."onChange={(event) => {setRoom(event.target.value);}}/>
      <button onClick= {joinRoom}>Join the Room</button>
+     </div>
+    )
+    : 
+    (
      <Chat socket = {socket} username = {username} room = {room} />
+    )}
     </div>
   );
 }
